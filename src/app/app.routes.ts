895b65ed72bce22302c05802ae8c99265c812141
Ricsius <hajnal.ricsi@gmail.com>
2025-07-07 17:35:03 +0200

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { MainMenu } from './main-menu/main-menu';
+
+export const routes: Routes = [
+    { path: '', component: MainMenu },
+    //{ path: 'second-component', component: SecondComponent },
+];
---
+++ src/app/main-menu/main-menu.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-main-menu',
+  imports: [
+    MatGridListModule,
+    MatButtonModule
+  ],
+  templateUrl: './main-menu.html',
+  styleUrl: './main-menu.css'
+})
+export class MainMenu {
+
+}
